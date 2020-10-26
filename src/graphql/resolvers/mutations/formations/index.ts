3b8a23b13a6f62b9695b573e71586e00936be46a
Houadria Hamza <hhouadria@toolynk.com>
2020-10-26 22:25:59 +0100

    push msg

--- src/graphql/resolvers/mutations/formations/index.ts
+++ src/graphql/resolvers/mutations/formations/index.ts
@@ -5,26 +5,29 @@ export default {
     new Promise((resolve, reject) => {
       const { name, description, file } = input;
       let inputs: any = { name, description };
-      console.log({inputs});
-      onesignal.sendMessage({
-        contents: {en:'Hello world!'}, 
-        included_segments:['All']
-    }, function(err, resp) {
-        console.log({resp})
-        if(err) {
+      console.log({ inputs });
+      onesignal.sendMessage(
+        {
+          contents: { en: name },
+          included_segments: ["All"],
+        },
+        function (err, resp) {
+          console.log({ resp });
+          if (err) {
             // Handle error
-        } else {
+          } else {
             // Handle success!
+          }
         }
-    });
-    console.log({input})
+      );
+      console.log({ input });
       new FormationModel({ ...inputs }).save((err, res) => {
-          resolve(res);
+        resolve(res);
       });
     }),
-    deleteFormation:(root,{id})=>{
-        return FormationModel.findById(id).then((formations:any)=>{
-            return Promise.resolve(formations.remove());
-        })
-    }
+  deleteFormation: (root, { id }) => {
+    return FormationModel.findById(id).then((formations: any) => {
+      return Promise.resolve(formations.remove());
+    });
+  },
 };
